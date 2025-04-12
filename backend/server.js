bdbc276d83b8aac1fa2e951051c0e029f3b910a1
Shivam Kasera <skshivamkasera2003@gmail.com>
2025-04-13 00:29:39 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,14 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", routes);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
